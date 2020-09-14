e9bd8412decdf42bf4de09b31cf69a1d93b18c54
Benjamin PERNEY <benjamin.perney@etam.fr>
2020-09-14 15:16:49 +0200

    Added style + children routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroTestComponent } from './hero-detail/hero-test/hero-test.component';
+import { HeroChildComponent } from './hero-detail/hero-child/hero-child.component';
+// import { HeroChildComponent } from './test/hero-child/hero-child.component';
+// import { HeroChild2Component } from './test/hero-child2/hero-second.component';
 
 
 
@@ -12,7 +16,17 @@ const routes: Routes = [
     path: 'heroes', component: HeroesListComponent
   },
   {
-    path: 'heroes/hero/:heroId', component: HeroDetailComponent
+    path: 'heroes/hero/:heroId', component: HeroDetailComponent,
+    children: [
+      {
+        path: 'herochild',
+        component: HeroTestComponent
+      },
+      {
+        path: 'herochild2',
+        component: HeroChildComponent
+      }
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,12 @@ import { FormsModule } from '@angular/forms';
 import { PowerBoostComponent } from './power-boost/power-boost.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroTestComponent } from './hero-detail/hero-test/hero-test.component';
+import { HeroChildComponent } from './hero-detail/hero-child/hero-child.component';
+// import { HeroChildComponent } from './test/hero-child/hero-child.component';
+// import { HeroChild2Component } from './test/hero-child2/hero-second.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +20,9 @@ import { DashboardComponent } from './dashboard/dashboard.component'
     PowerBoostComponent,
     HeroDetailComponent,
     HeroesListComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroTestComponent,
+    HeroChildComponent,
   ],
   imports: [
     BrowserModule,
